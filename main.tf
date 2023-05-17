a2cd67df06caddd6cadb50f5aca11f7922b4c141
mcgonagle <mcgonagle@gmail.com>
2023-05-17 10:25:03 -0500

    adding main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+variable "api_token" {}
+variable "zone_id" {}
+
+terraform {
+  required_providers {
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "~> 4"
+    }
+  }
+}
+
+provider "cloudflare" {
+  api_token = var.api_token
+}
+
+resource "cloudflare_record" "www" {
+  zone_id = var.zone_id
+  name    = "www"
+  value   = "203.0.113.10"
+  type    = "A"
+  proxied = true
+}
