583fdb5cda538304a315c66961a9fddb16bfef19
mcgonagle <mcgonagle@gmail.com>
2023-10-03 08:51:17 -0400

    made the bucket name a little bit more random

--- demos/demo8/main.tf
+++ demos/demo8/main.tf
@@ -1,8 +1,8 @@
 resource "aws_s3_bucket" "example" {
-  bucket = "toms-test-bucket-1234"
+  bucket = "toms-test-bucket-123456789"
 
   tags = {
-    Name        = "Toms Test Bucket 1234"
+    Name        = "Toms Test Bucket 123456789"
     Environment = "Dev"
   }
 }
