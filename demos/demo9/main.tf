d9bc8ef93040b0928c1c1cc31c61eda642a53637
mcgonagle <mcgonagle@gmail.com>
2023-10-03 09:06:03 -0400

    added more azs to vpc

--- demos/demo9/main.tf
+++ demos/demo9/main.tf
@@ -1,4 +1,20 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.1.2"
+
+  name = "toms-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-east-1a", "eu-east-1b", "eu-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
+
